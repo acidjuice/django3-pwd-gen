f1eb9d3c7218663e4168fcffa97d07c5f2e5be6b
acidjuice <acidjuice123@gmail.com>
2021-08-31 18:22:12 +0300

    initial commit pwd gen site

---
+++ generator/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+def home(request):
+    return render (request, 'generator/home.html',)
+
+def pwd(request):
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()_+=№;%:?'))
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+    lenght = int(request.GET.get('lenght'))
+
+    thepassword = ''
+
+    for x in range(lenght):
+        thepassword += random.choice(characters)
+
+
+
+    return render (request, 'generator/pwd.html', {'pwd':thepassword})
+
+def about(request):
+    return render (request, 'generator/about.html')
